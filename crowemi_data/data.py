abe6e9cd14bf146ba8fc11cf4f28ac96b806e44d
Andy Crowe <crowemi@hotmail.com>
2025-02-16 20:50:59 -0800

    added async data

--- crowemi_data/data.py
+++ crowemi_data/data.py
@@ -2,15 +2,61 @@ import uuid
 from pymongo import MongoClient, AsyncMongoClient
 
 
+class AsnyMongoDBClient():
+    def __init__(self, uri: str, database: str, session_id: str = None):
+
+        if not session_id:
+            session_id = uuid.uuid4().hex
+        self.session_id = session_id
+
+        self.client: AsyncMongoClient = AsyncMongoClient(uri)
+
+        self.db = self.client.get_database(database)
+
+    async def read(self, collection: str, query: dict):
+        try:
+            ret = list()
+            database = self.client.get_database(self.db)
+            collection = database.get_collection(collection)
+            res = await collection.find(query)
+            [ret.append(doc) for doc in res]
+            return ret
+        except Exception as e:
+            raise e
+
+    async def delete(self, collection: str, query: dict):
+        try:
+            database = self.client.get_database(self.db)
+            collection = database.get_collection(collection)
+            return await collection.delete_many(query)
+        except Exception as e:
+            raise e
+
+    async def create(self, collection: str, data: dict):
+        try:
+            database = self.client.get_database(self.db)
+            collection = database.get_collection(collection)
+            return await collection.insert_one(data).inserted_id
+        except Exception as e:
+            raise e
+
+    async def update(self, collection: str, query: dict, data: dict):
+        try:
+            database = self.client.get_database(self.db)
+            collection = database.get_collection(collection)
+            return await collection.update_one(filter=query, update=data)
+        except Exception as e:
+            raise e
+
+
 class MongoDBClient():
-    def __init__(self, uri: str, database: str = "crowemi-trades", session_id: str = None):
+    def __init__(self, uri: str, database: str, session_id: str = None):
 
         if not session_id:
             session_id = uuid.uuid4().hex
         self.session_id = session_id
 
         self.client: MongoClient = MongoClient(uri)
-        self.async_client: AsyncMongoClient = AsyncMongoClient(uri)
 
         self.db = self.client.get_database(database)
 
@@ -18,17 +64,6 @@ class MongoDBClient():
         try:
             ret = list()
             res = self.db.get_collection(collection).find(query)
-            [ret.append(doc) for doc in res]
-            return ret
-        except Exception as e:
-            raise e
-
-    async def read(self, collection: str, query: dict):
-        try:
-            ret = list()
-            database = self.async_client.get_database(self.db)
-            collection = database.get_collection(collection)
-            res = await collection.find(query)
             [ret.append(doc) for doc in res]
             return ret
         except Exception as e:
@@ -42,14 +77,6 @@ class MongoDBClient():
         except Exception as e:
             raise e
 
-    async def create(self, collection: str, data: dict):
-        try:
-            database = self.async_client.get_database(self.db)
-            collection = database.get_collection(collection)
-            return await collection.insert_one(data).inserted_id
-        except Exception as e:
-            raise e
-
     def update(self, collection: str, query: dict, data: dict):
         try:
             database = self.client.get_database(self.db)
@@ -58,14 +85,6 @@ class MongoDBClient():
         except Exception as e:
             raise e
 
-    async def update(self, collection: str, query: dict, data: dict):
-        try:
-            database = self.async_client.get_database(self.db)
-            collection = database.get_collection(collection)
-            return await collection.update_one(filter=query, update=data)
-        except Exception as e:
-            raise e
-        
     def delete(self, collection: str, query: dict):
         try:
             database = self.client.get_database(self.db)
@@ -74,10 +93,3 @@ class MongoDBClient():
         except Exception as e:
             raise e
 
-    async def delete(self, collection: str, query: dict):
-        try:
-            database = self.async_client.get_database(self.db)
-            collection = database.get_collection(collection)
-            return await collection.delete_many(query)
-        except Exception as e:
-            raise e
