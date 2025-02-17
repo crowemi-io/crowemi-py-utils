b7049825c7506909809e562de2b548011f7658c5
Andy Crowe <crowemi@hotmail.com>
2025-02-17 07:28:16 -0800

    added tests for sync mongo

--- crowemi_data/data.py
+++ crowemi_data/data.py
@@ -18,7 +18,16 @@ class AsyncMongoDBClient(Base):
         super().__init__(database, session_id)
         self.client: AsyncMongoClient = AsyncMongoClient(uri, maxPoolSize=MAX_POOL_SIZE, minPoolSize=MIN_POOL_SIZE, maxIdleTimeMS = MAX_IDLE_TIME)
 
-    async def read(self, collection: str, query: dict):
+    async def create(self, collection: str, data: dict) -> str:
+        try:
+            database = self.client.get_database(self.database)
+            collection = database.get_collection(collection)
+            ret = await collection.insert_one(data)
+            return ret.inserted_id
+        except Exception as e:
+            raise e
+
+    async def read(self, collection: str, query: dict) -> list:
         try:
             ret = list()
             database = self.client.get_database(self.database)
@@ -28,30 +37,21 @@ class AsyncMongoDBClient(Base):
         except Exception as e:
             raise e
 
-    async def delete(self, collection: str, query: dict):
-        try:
-            database = self.client.get_database(self.database)
-            collection = database.get_collection(collection)
-            ret = await collection.delete_many(query)
-            return ret.deleted_count
-        except Exception as e:
-            raise e
-
-    async def create(self, collection: str, data: dict):
-        try:
-            database = self.client.get_database(self.database)
-            collection = database.get_collection(collection)
-            ret = await collection.insert_one(data)
-            return ret.inserted_id
-        except Exception as e:
-            raise e
-
-    async def update(self, collection: str, query: dict, data: dict):
+    async def update(self, collection: str, query: dict, data: dict) -> int:
         try:
             database = self.client.get_database(self.database)
             collection = database.get_collection(collection)
             ret = await collection.update_one(filter=query, update=data)
             return ret.modified_count
+        except Exception as e:
+            raise e
+
+    async def delete(self, collection: str, query: dict) -> int:
+        try:
+            database = self.client.get_database(self.database)
+            collection = database.get_collection(collection)
+            ret = await collection.delete_many(query)
+            return ret.deleted_count
         except Exception as e:
             raise e
 
@@ -61,36 +61,39 @@ class MongoDBClient(Base):
         super().__init__(database, session_id)
         self.client: MongoClient = MongoClient(uri, maxPoolSize=MAX_POOL_SIZE, minPoolSize=MIN_POOL_SIZE, maxIdleTimeMS = MAX_IDLE_TIME)
 
-    def read(self, collection: str, query: dict):
+    def create(self, collection: str, data: dict) -> str:
         try:
-            ret = list()
-            res = self.db.get_collection(collection).find(query)
-            [ret.append(doc) for doc in res]
+            database = self.client.get_database(self.database)
+            collection = database.get_collection(collection)
+            ret = collection.insert_one(data)
+            return ret.inserted_id
+        except Exception as e:
+            raise e
+
+    def read(self, collection: str, query: dict) -> list:
+        try:
+            database = self.client.get_database(self.database)
+            collection = database.get_collection(collection)
+            ret = collection.find(query).to_list(length=None)
             return ret
         except Exception as e:
             raise e
 
-    def create(self, collection: str, data: dict):
+    def update(self, collection: str, query: dict, data: dict) -> int:
         try:
-            database = self.client.get_database(self.db)
+            database = self.client.get_database(self.database)
             collection = database.get_collection(collection)
-            return collection.insert_one(data).inserted_id
+            ret = collection.update_one(filter=query, update=data)
+            return ret.modified_count
         except Exception as e:
             raise e
 
-    def update(self, collection: str, query: dict, data: dict):
+    def delete(self, collection: str, query: dict) -> int:
         try:
-            database = self.client.get_database(self.db)
+            database = self.client.get_database(self.database)
             collection = database.get_collection(collection)
-            return collection.update_one(filter=query, update=data)
-        except Exception as e:
-            raise e
-
-    def delete(self, collection: str, query: dict):
-        try:
-            database = self.client.get_database(self.db)
-            collection = database.get_collection(collection)
-            return collection.delete_many(query)
+            ret = collection.delete_many(query)
+            return ret.deleted_count
         except Exception as e:
             raise e
 
--- tests/test_data.py
+++ tests/test_data.py
@@ -7,7 +7,6 @@ from bson import ObjectId
 from crowemi_data.data import AsyncMongoDBClient, MongoDBClient
 
 
-
 class TestAsyncMongoDBClient(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.client = AsyncMongoDBClient(uri="mongodb://root:example@localhost:27017/", database="test_db")
@@ -32,6 +31,29 @@ class TestAsyncMongoDBClient(unittest.IsolatedAsyncioTestCase):
         ret = await self.client.update(collection="test_collection", query={"_id": object_id}, data={'$set': {"test": "updated_test"}})
         self.assertTrue(ret > 0)
 
+class TestMongoDBClient(unittest.TestCase):
+    def setUp(self):
+        self.client = MongoDBClient(uri="mongodb://root:example@localhost:27017/", database="test_db")
+
+    def test_create(self):
+        ret = self.client.create(collection="test_collection", data={"test": "test"})
+        self.assertTrue(type(ret) == ObjectId)
+        return ret
+
+    def test_read(self):
+        object_id = self.test_create()
+        ret = self.client.read(collection="test_collection", query={"_id": object_id})
+        self.assertTrue(len(ret) > 0)
+
+    def test_delete(self):
+        object_id = self.test_create()
+        ret = self.client.delete(collection="test_collection", query={"_id": object_id})
+        self.assertTrue(ret > 0)
+
+    def test_update(self):
+        object_id = self.test_create()
+        ret = self.client.update(collection="test_collection", query={"_id": object_id}, data={'$set': {"test": "updated_test"}})
+        self.assertTrue(ret > 0)
 
 
 if __name__ == '__main__':
