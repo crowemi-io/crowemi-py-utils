c93a4b4a43338e7fb13fc96f06db50c4bf546181
Andy Crowe <crowemi@hotmail.com>
2025-03-31 06:42:30 -0700

    standard headers

--- crowemi_model/helper.py
+++ crowemi_model/helper.py
@@ -15,6 +15,25 @@ class CrowemiHeaders(BaseModel):
     crowemi_client_secret_key: str
     crowemi_session_id: str | None = None
 
+    def create_headers(self) -> dict:
+        """
+        Creates and returns a dictionary of headers for HTTP requests within the crowemi eco-system.
+        The headers include:
+        - `crowemi-client-id`: The client ID associated with the crowemi service.
+        - `crowemi-client-secret-key`: The secret key for the crowemi client.
+        - `crowmei-client-name`: The name of the Crowemi client.
+        - `Content-Type`: Specifies the content type as JSON.
+        Returns:
+            dict: A dictionary containing the headers for HTTP requests.
+        """
+
+        return {
+            "crowemi-client-id": self.crowemi_client_id,
+            "crowemi-client-secret-key": self.crowemi_client_secret_key,
+            "crowmei-client-name": self.crowemi_client_name,
+            "Content-Type": "application/json"
+        }
+
 class Helper():
     @staticmethod
     def convert_config(b64: str | None) -> dict | None:
