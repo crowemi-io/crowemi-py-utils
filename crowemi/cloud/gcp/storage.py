b1634b046d9f7e0b177cb0dbe823df3699ca2e39
Andy Crowe <crowemi@hotmail.com>
2025-07-22 20:22:37 -0700

    added storage + fix log params

---
+++ crowemi/cloud/gcp/storage.py
@@ -0,0 +1,12 @@
+from google.cloud import storage
+
+def write(bucket_name: str, prefix: str, file_name: str, content: str, content_type: str = 'application/jsonl'):
+    try:
+        # Initialize a storage client
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(file_name)
+        # Upload the content to GCS
+        blob.upload_from_string(content, content_type=content_type)
+    except Exception as e:
+        return e
